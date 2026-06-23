7b08186556b3ba82c53ef72d4bfb939760978933
Vanshita Khem chandani <vanshiitaa25@gmail.com>
2026-06-23 20:29:40 +0530

    fix: changed main.jsx path to relative in index.html

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    // Yeh line strict checking ko warning me badal degi, jisse build fail nahi hogi
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
-      // Yeh sabse zaroori line hai: agar koi cycle dikhegi toh build nahi rukegi
-      onwarn(warning, warn) {
-        if (warning.code === 'CYCLIC_CROSS_CHUNK_REPROJECTION' || warning.code === 'CIRCULAR_DEPENDENCY') return;
-        warn(warning);
-      }
-    },
-  },
+  base: './', // Yeh Vite ko pure project ke paths relative rakhne ko bolega
 });
